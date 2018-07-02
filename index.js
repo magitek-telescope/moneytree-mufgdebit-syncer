35255c0ad3407ea06d0c6f48a87f3005539f3498
HANATANI Takuma <mail@potato4d.me>
2018-07-03 00:36:47 +0900

    :recycle:

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const delay = require('delay')
-const puppeteer = require('puppeteer');
+const delay = require("delay");
+const puppeteer = require("puppeteer");
 
 const {
   MONEYTREE_TOKEN,
@@ -7,68 +7,78 @@ const {
   MUFG_ID,
   MUFG_PASSWORD,
   TARGET_MONTH
-} = process.env
+} = process.env;
 
-let result = [['ID', '用途', '金額', '日付']]
+let result = [["ID", "用途", "金額", "日付"]];
 
 async function scrapingFromMUFG() {
-  const browser = await puppeteer.launch({ headless: false })
-  let pages, page, isEnded
+  const browser = await puppeteer.launch({ headless: false });
+  let pages, page, isEnded;
 
-  page = await browser.newPage()
-  await page.goto('https://entry11.bk.mufg.jp/ibg/dfw/APLIN/loginib/login?_TRANID=AA000_001')
-  await delay(150)
-  await page.type('#account_id', MUFG_ID)
-  await page.type('#ib_password', MUFG_PASSWORD)
-  await delay(150)
-  await page.click('.acenter.admb_m > a')
-  await page.waitForNavigation()
-  await page.click('#list > li:nth-child(10)')
-  await page.waitForNavigation()
-  await delay(300)
-  await page.click('[alt="VISAデビット会員用Webにログインする"]')
-  await delay(2000)
+  page = await browser.newPage();
+  await page.goto(
+    "https://entry11.bk.mufg.jp/ibg/dfw/APLIN/loginib/login?_TRANID=AA000_001"
+  );
+  await delay(150);
+  await page.type("#account_id", MUFG_ID);
+  await page.type("#ib_password", MUFG_PASSWORD);
+  await delay(150);
+  await page.click(".acenter.admb_m > a");
+  await page.waitForNavigation();
+  await page.click("#list > li:nth-child(10)");
+  await page.waitForNavigation();
+  await delay(300);
+  await page.click('[alt="VISAデビット会員用Webにログインする"]');
+  await delay(2000);
 
-  pages = await browser.pages()
-  page = pages[pages.length-1]
-  await page.click('.nav-02 a')
-  await page.waitForNavigation()
-  await delay(500)
-  await page.select('[name="W031301.referenceDate"]', TARGET_MONTH)
-  await delay(1000)
+  pages = await browser.pages();
+  page = pages[pages.length - 1];
+  await page.click(".nav-02 a");
+  await page.waitForNavigation();
+  await delay(500);
+  await page.select('[name="W031301.referenceDate"]', TARGET_MONTH);
+  await delay(1000);
 
   while (!isEnded) {
-    result.push(...(await page.evaluate(async () => {
-      let cols = []
-      document.querySelectorAll('.lyt-sp-none .tbl-02 tbody').forEach((col) => {
-        cols.push([
-          `VISA-${col.querySelector('td:nth-of-type(9)').innerHTML}`,
-          `${col.querySelector('td:nth-of-type(2)').innerHTML}`,
-          `${col.querySelector('td:nth-of-type(3)').innerHTML.replace(/ /g, '')}`,
-          `${col.querySelector('td:nth-of-type(1)').innerHTML}`
-        ].join(',').replace(/&nbsp;/g, '&').replace(/\n/g, ''))
-      })
-      return cols
-    })))
+    result.push(
+      ...(await page.evaluate(async () => {
+        let cols = [];
+        document.querySelectorAll(".lyt-sp-none .tbl-02 tbody").forEach(col => {
+          cols.push(
+            [
+              `VISA-${col.querySelector("td:nth-of-type(9)").innerHTML}`,
+              `${col.querySelector("td:nth-of-type(2)").innerHTML}`,
+              `${col
+                .querySelector("td:nth-of-type(3)")
+                .innerHTML.replace(/ /g, "")}`,
+              `${col.querySelector("td:nth-of-type(1)").innerHTML}`
+            ]
+              .join(",")
+              .replace(/&nbsp;/g, "&")
+              .replace(/\n/g, "")
+          );
+        });
+        return cols;
+      }))
+    );
     isEnded = await page.evaluate(() => {
-      return document.querySelector('.nablarch_nextSubmit').classList.contains('is-disabled')
-    })
+      return document
+        .querySelector(".nablarch_nextSubmit")
+        .classList.contains("is-disabled");
+    });
     if (!isEnded) {
-      await page.click('.nablarch_nextSubmit')
-      await page.waitForNavigation()
-      await delay(500)
+      await page.click(".nablarch_nextSubmit");
+      await page.waitForNavigation();
+      await delay(500);
     }
   }
-  console.log(result.join("\n"))
+  console.log(result.join("\n"));
   await browser.close();
 }
 
-async function postToMoneyTree() {
+async function postToMoneyTree() {}
 
-}
-
-
-;(async () => {
-  await scrapingFromMUFG()
-  await postToMoneyTree()
-})()
+(async () => {
+  await scrapingFromMUFG();
+  await postToMoneyTree();
+})();
